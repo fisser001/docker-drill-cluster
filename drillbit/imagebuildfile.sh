a8191455277c1d0be386bdefafeaa2f6207fecc7
Fisser <mfisser@sva.de>
2019-03-18 13:56:16 +0100

    initial

--- drillbit/imagebuildfile.sh
+++ drillbit/imagebuildfile.sh
@@ -2,18 +2,6 @@
 
 export version=1.0.0
 
-cd ~/repos/masterarbeit/docker-presto-cluster/base
-docker build -t mfisser/presto-base:${version} ./
-docker push mfisser/presto-base:${version}
-
-cd ~/repos/masterarbeit/docker-presto-cluster/coordinator
-docker build -t mfisser/presto-coordinator:${version} ./
-docker push mfisser/presto-coordinator:${version}
-
-cd ~/repos/masterarbeit/docker-presto-cluster/worker
-docker build -t mfisser/presto-worker:${version} ./
-docker push mfisser/presto-worker:${version}
-
-cd ~/repos/masterarbeit/docker-presto-cluster/cli
-docker build -t mfisser/presto-cli:${version} ./
-docker push mfisser/presto-cli:${version}
+cd ~/repos/masterarbeit/docker-drill-cluster/drillbit
+docker build -t mfisser/drillbit:${version} ./
+docker push mfisser/drillbit:${version}
